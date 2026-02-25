cb2b4364bb204595012cfcdcfd3ac88ebc3e65be
Rubén Abad <rubenabad89@gmail.com>
2026-02-25 18:47:40 +0100

    chore: remove temp playwright config

--- playwright-api.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig, devices } from '@playwright/test';
-export default defineConfig({
-  testDir: './src/tests/e2e',
-  testMatch: '**/meal-plan-generation.e2e.ts',
-  timeout: 120000,
-  use: { ignoreHTTPSErrors: true },
-  projects: [{ name: 'chromium', use: { ...devices['Desktop Chrome'] } }],
-});
